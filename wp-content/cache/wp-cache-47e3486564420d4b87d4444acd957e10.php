39616e0c46a90238af08a4cbf5d46c22f81e7233
Purnima <purnimadev@hotmail.com>
2017-04-17 22:50:53 -0700

    Adding files

---
+++ wp-content/cache/wp-cache-47e3486564420d4b87d4444acd957e10.php
@@ -0,0 +1,219 @@
+<?php die(); ?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="en-US">
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html lang="en-US">
+<!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Coroflot Advertising | </title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="http://localhost:8888/accelerate/xmlrpc.php">
+	<!--[if lt IE 9]>
+	<script src="http://localhost:8888/accelerate/wp-content/themes/accelerate-theme/js/html5.js"></script>
+	<![endif]-->
+
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,700,400' rel='stylesheet' type='text/css'>
+<link href="http://localhost:8888/accelerate/wp-content/themes/accelerate-child-theme/style.css" rel="stylesheet" type='text/css'>
+	<link rel='dns-prefetch' href='//s.w.org' />
+		<script type="text/javascript">
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.2.1\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/localhost:8888\/accelerate\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.7.3"}};
+			!function(a,b,c){function d(a){var b,c,d,e,f=String.fromCharCode;if(!k||!k.fillText)return!1;switch(k.clearRect(0,0,j.width,j.height),k.textBaseline="top",k.font="600 32px Arial",a){case"flag":return k.fillText(f(55356,56826,55356,56819),0,0),!(j.toDataURL().length<3e3)&&(k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,65039,8205,55356,57096),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57331,55356,57096),0,0),c=j.toDataURL(),b!==c);case"emoji4":return k.fillText(f(55357,56425,55356,57341,8205,55357,56507),0,0),d=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55357,56425,55356,57341,55357,56507),0,0),e=j.toDataURL(),d!==e}return!1}function e(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i,j=b.createElement("canvas"),k=j.getContext&&j.getContext("2d");for(i=Array("flag","emoji4"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+		</script>
+		<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost:8888/accelerate/wp-includes/css/dashicons.min.css?ver=4.7.3' type='text/css' media='all' />
+<link rel='stylesheet' id='admin-bar-css'  href='http://localhost:8888/accelerate/wp-includes/css/admin-bar.min.css?ver=4.7.3' type='text/css' media='all' />
+<link rel='stylesheet' id='PI_stt_front-css'  href='http://localhost:8888/accelerate/wp-content/plugins/simple-twitter-tweets/css/stt.min.css?ver=4.7.3' type='text/css' media='all' />
+<link rel='stylesheet' id='A2A_SHARE_SAVE-css'  href='http://localhost:8888/accelerate/wp-content/plugins/add-to-any/addtoany.min.css?ver=1.13' type='text/css' media='all' />
+<script type='text/javascript' src='http://localhost:8888/accelerate/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+<script type='text/javascript' src='http://localhost:8888/accelerate/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script type='text/javascript' src='http://localhost:8888/accelerate/wp-content/plugins/add-to-any/addtoany.min.js?ver=1.0'></script>
+<link rel='https://api.w.org/' href='http://localhost:8888/accelerate/wp-json/' />
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://localhost:8888/accelerate/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://localhost:8888/accelerate/wp-includes/wlwmanifest.xml" /> 
+<link rel='next' title='Zurb Foundation Project' href='http://localhost:8888/accelerate/case-studies/zurb__trashed/' />
+<meta name="generator" content="WordPress 4.7.3" />
+<link rel="canonical" href="http://localhost:8888/accelerate/case-studies/coroloft/" />
+<link rel='shortlink' href='http://localhost:8888/accelerate/?p=48' />
+<link rel="alternate" type="application/json+oembed" href="http://localhost:8888/accelerate/wp-json/oembed/1.0/embed?url=http%3A%2F%2Flocalhost%3A8888%2Faccelerate%2Fcase-studies%2Fcoroloft%2F" />
+<link rel="alternate" type="text/xml+oembed" href="http://localhost:8888/accelerate/wp-json/oembed/1.0/embed?url=http%3A%2F%2Flocalhost%3A8888%2Faccelerate%2Fcase-studies%2Fcoroloft%2F&#038;format=xml" />
+
+<script type="text/javascript">
+var a2a_config=a2a_config||{};a2a_config.callbacks=a2a_config.callbacks||[];a2a_config.templates=a2a_config.templates||{};
+</script>
+<script type="text/javascript" src="http://static.addtoany.com/menu/page.js" async="async"></script>
+		<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+		<style type="text/css" media="print">#wpadminbar { display:none; }</style>
+<style type="text/css" media="screen">
+	html { margin-top: 32px !important; }
+	* html body { margin-top: 32px !important; }
+	@media screen and ( max-width: 782px ) {
+		html { margin-top: 46px !important; }
+		* html body { margin-top: 46px !important; }
+	}
+</style>
+</head>
+
+<body class="case_studies-template-default single single-case_studies postid-48 logged-in admin-bar no-customize-support">
+<div id="page" class="hfeed site">
+	<header id="masthead" class="site-header" role="banner">
+		<div class="header-main">
+			<h1 class="site-title"><a href="http://localhost:8888/accelerate/" rel="home">Accelerate</a></h1>
+
+			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+				<div class="menu-primary-menu-container"><ul id="menu-primary-menu" class="nav-menu"><li id="menu-item-12" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-12"><a href="http://localhost:8888/accelerate/sample-page/">ABOUT</a></li>
+<li id="menu-item-13" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-13"><a href="http://localhost:8888/accelerate/sample-page/">WORK</a></li>
+<li id="menu-item-14" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-14"><a href="http://localhost:8888/accelerate/sample-page/">BLOG</a></li>
+<li id="menu-item-15" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-15"><a href="http://localhost:8888/accelerate/sample-page/">CONTACT</a></li>
+</ul></div>			</nav>
+			<div class="clearfix"></div>
+		</div>
+
+	</header><!-- #masthead -->
+
+	<div id="main" class="site-main">
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+		
+			
+<article class="case-study">		
+			
+	<aside class="case-study-sidebar">
+		<h2>Coroflot Advertising</h2>
+		<h5>Influencer Mapping, Social Media Strategy</h5>
+		<h6>Client: </h6>
+		
+		<p>This is a project I made for this company for this. Here is the early staget startup that I helped design and do front-end and back-end dev for.</p>
+<div class="addtoany_share_save_container addtoany_content_bottom"><div class="a2a_kit a2a_kit_size_32 addtoany_list" data-a2a-url="http://localhost:8888/accelerate/case-studies/coroloft/" data-a2a-title="Coroflot Advertising"><a class="a2a_button_facebook" href="http://www.addtoany.com/add_to/facebook?linkurl=http%3A%2F%2Flocalhost%3A8888%2Faccelerate%2Fcase-studies%2Fcoroloft%2F&amp;linkname=Coroflot%20Advertising" title="Facebook" rel="nofollow" target="_blank"></a><a class="a2a_button_twitter" href="http://www.addtoany.com/add_to/twitter?linkurl=http%3A%2F%2Flocalhost%3A8888%2Faccelerate%2Fcase-studies%2Fcoroloft%2F&amp;linkname=Coroflot%20Advertising" title="Twitter" rel="nofollow" target="_blank"></a><a class="a2a_button_google_plus" href="http://www.addtoany.com/add_to/google_plus?linkurl=http%3A%2F%2Flocalhost%3A8888%2Faccelerate%2Fcase-studies%2Fcoroloft%2F&amp;linkname=Coroflot%20Advertising" title="Google+" rel="nofollow" target="_blank"></a><a class="a2a_dd addtoany_share_save" href="https://www.addtoany.com/share"></a></div></div>		
+		<p><a href="">Site Link</a></p>
+
+
+	</aside>
+
+
+
+
+<div class="case-study-images">
+
+		<img width="514" height="379" src="http://localhost:8888/accelerate/wp-content/uploads/2017/03/coroloft-1.jpg" class="attachment-full size-full" alt="" srcset="http://localhost:8888/accelerate/wp-content/uploads/2017/03/coroloft-1.jpg 514w, http://localhost:8888/accelerate/wp-content/uploads/2017/03/coroloft-1-300x221.jpg 300w" sizes="(max-width: 514px) 100vw, 514px" /> 
+		
+			
+			
+	
+
+</div>
+		
+		</article>
+			
+		
+		
+		
+		
+			
+
+			
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+		</div><!-- #main -->
+
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<div class="site-description">
+				<p>Accelerate Your Marketing</p>
+				<p>&copy; Accelerate, LLC
+				</div>
+				
+			<nav class="social-media-navigation" role="navigation">
+				<div class="social-media-menu"><ul>
+<li class="page_item page-item-23"><a href="http://localhost:8888/accelerate/contact-us/">Contact Us</a></li>
+<li class="page_item page-item-117"><a href="http://localhost:8888/accelerate/about-us/">Our Services</a></li>
+<li class="page_item page-item-2"><a href="http://localhost:8888/accelerate/sample-page/">Sample Page</a></li>
+<li class="page_item page-item-90"><a href="http://localhost:8888/accelerate/test/">test</a></li>
+</ul></div>
+			</nav>
+
+				
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<script type='text/javascript' src='http://localhost:8888/accelerate/wp-includes/js/admin-bar.min.js?ver=4.7.3'></script>
+<script type='text/javascript' src='http://localhost:8888/accelerate/wp-includes/js/wp-embed.min.js?ver=4.7.3'></script>
+	<!--[if lte IE 8]>
+		<script type="text/javascript">
+			document.body.className = document.body.className.replace( /(^|\s)(no-)?customize-support(?=\s|$)/, '' ) + ' no-customize-support';
+		</script>
+	<![endif]-->
+	<!--[if gte IE 9]><!-->
+		<script type="text/javascript">
+			(function() {
+				var request, b = document.body, c = 'className', cs = 'customize-support', rcs = new RegExp('(^|\\s+)(no-)?'+cs+'(\\s+|$)');
+
+						request = true;
+		
+				b[c] = b[c].replace( rcs, ' ' );
+				// The customizer requires postMessage and CORS (if the site is cross domain)
+				b[c] += ( window.postMessage && request ? ' ' : ' no-' ) + cs;
+			}());
+		</script>
+	<!--<![endif]-->
+			<div id="wpadminbar" class="nojq nojs">
+							<a class="screen-reader-shortcut" href="#wp-toolbar" tabindex="1">Skip to toolbar</a>
+						<div class="quicklinks" id="wp-toolbar" role="navigation" aria-label="Toolbar" tabindex="0">
+				<ul id="wp-admin-bar-root-default" class="ab-top-menu">
+		<li id="wp-admin-bar-wp-logo" class="menupop"><a class="ab-item" aria-haspopup="true" href="http://localhost:8888/accelerate/wp-admin/about.php"><span class="ab-icon"></span><span class="screen-reader-text">About WordPress</span></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-wp-logo-default" class="ab-submenu">
+		<li id="wp-admin-bar-about"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/about.php">About WordPress</a>		</li></ul><ul id="wp-admin-bar-wp-logo-external" class="ab-sub-secondary ab-submenu">
+		<li id="wp-admin-bar-wporg"><a class="ab-item" href="https://wordpress.org/">WordPress.org</a>		</li>
+		<li id="wp-admin-bar-documentation"><a class="ab-item" href="https://codex.wordpress.org/">Documentation</a>		</li>
+		<li id="wp-admin-bar-support-forums"><a class="ab-item" href="https://wordpress.org/support/">Support Forums</a>		</li>
+		<li id="wp-admin-bar-feedback"><a class="ab-item" href="https://wordpress.org/support/forum/requests-and-feedback">Feedback</a>		</li></ul></div>		</li>
+		<li id="wp-admin-bar-site-name" class="menupop"><a class="ab-item" aria-haspopup="true" href="http://localhost:8888/accelerate/wp-admin/">Accelerate</a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-site-name-default" class="ab-submenu">
+		<li id="wp-admin-bar-dashboard"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/">Dashboard</a>		</li></ul><ul id="wp-admin-bar-appearance" class="ab-submenu">
+		<li id="wp-admin-bar-themes"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/themes.php">Themes</a>		</li>
+		<li id="wp-admin-bar-widgets"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/widgets.php">Widgets</a>		</li>
+		<li id="wp-admin-bar-menus"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/nav-menus.php">Menus</a>		</li></ul></div>		</li>
+		<li id="wp-admin-bar-customize" class="hide-if-no-customize"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/customize.php?url=http%3A%2F%2Flocalhost%3A8888%2Faccelerate%2Fcase-studies%2Fcoroloft%2F">Customize</a>		</li>
+		<li id="wp-admin-bar-comments"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/edit-comments.php"><span class="ab-icon"></span><span class="ab-label awaiting-mod pending-count count-0" aria-hidden="true">0</span><span class="screen-reader-text">0 comments awaiting moderation</span></a>		</li>
+		<li id="wp-admin-bar-new-content" class="menupop"><a class="ab-item" aria-haspopup="true" href="http://localhost:8888/accelerate/wp-admin/post-new.php"><span class="ab-icon"></span><span class="ab-label">New</span></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-new-content-default" class="ab-submenu">
+		<li id="wp-admin-bar-new-post"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/post-new.php">Post</a>		</li>
+		<li id="wp-admin-bar-new-media"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/media-new.php">Media</a>		</li>
+		<li id="wp-admin-bar-new-page"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/post-new.php?post_type=page">Page</a>		</li>
+		<li id="wp-admin-bar-new-case_studies"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/post-new.php?post_type=case_studies">Case Study</a>		</li>
+		<li id="wp-admin-bar-new-services"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/post-new.php?post_type=services">Services</a>		</li>
+		<li id="wp-admin-bar-new-user"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/user-new.php">User</a>		</li></ul></div>		</li>
+		<li id="wp-admin-bar-edit"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/post.php?post=48&#038;action=edit">Edit Post</a>		</li></ul><ul id="wp-admin-bar-top-secondary" class="ab-top-secondary ab-top-menu">
+		<li id="wp-admin-bar-search" class="admin-bar-search"><div class="ab-item ab-empty-item" tabindex="-1"><form action="http://localhost:8888/accelerate/" method="get" id="adminbarsearch"><input class="adminbar-input" name="s" id="adminbar-search" type="text" value="" maxlength="150" /><label for="adminbar-search" class="screen-reader-text">Search</label><input type="submit" class="adminbar-button" value="Search"/></form></div>		</li>
+		<li id="wp-admin-bar-my-account" class="menupop with-avatar"><a class="ab-item" aria-haspopup="true" href="http://localhost:8888/accelerate/wp-admin/profile.php">Howdy, admin<img alt='' src='http://1.gravatar.com/avatar/4e3630f200dc8221ce57014f27c97b86?s=26&#038;d=mm&#038;r=g' srcset='http://1.gravatar.com/avatar/4e3630f200dc8221ce57014f27c97b86?s=52&amp;d=mm&amp;r=g 2x' class='avatar avatar-26 photo' height='26' width='26' /></a><div class="ab-sub-wrapper"><ul id="wp-admin-bar-user-actions" class="ab-submenu">
+		<li id="wp-admin-bar-user-info"><a class="ab-item" tabindex="-1" href="http://localhost:8888/accelerate/wp-admin/profile.php"><img alt='' src='http://1.gravatar.com/avatar/4e3630f200dc8221ce57014f27c97b86?s=64&#038;d=mm&#038;r=g' srcset='http://1.gravatar.com/avatar/4e3630f200dc8221ce57014f27c97b86?s=128&amp;d=mm&amp;r=g 2x' class='avatar avatar-64 photo' height='64' width='64' /><span class='display-name'>admin</span></a>		</li>
+		<li id="wp-admin-bar-edit-profile"><a class="ab-item" href="http://localhost:8888/accelerate/wp-admin/profile.php">Edit My Profile</a>		</li>
+		<li id="wp-admin-bar-logout"><a class="ab-item" href="http://localhost:8888/accelerate/wp-login.php?action=logout&#038;_wpnonce=cd600a692a">Log Out</a>		</li></ul></div>		</li></ul>			</div>
+						<a class="screen-reader-shortcut" href="http://localhost:8888/accelerate/wp-login.php?action=logout&#038;_wpnonce=cd600a692a">Log Out</a>
+					</div>
+
+		</body>
+</html>
+<!-- Dynamic page generated in 0.401 seconds. -->
+<!-- Cached page generated by WP-Super-Cache on 2017-04-18 05:44:43 -->
